289746e3d5ca8ea082f66da34555724c1cd7df99
Haluk Aytış <halukaytis@hotmail.com>
2024-07-26 17:18:13 +0300

    serviceRegistration eklendi

---
+++ WebApp.DataAccess/Services/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.DataAccess.Services
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
